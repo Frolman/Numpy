b72878878f1571ebb3df07b9bf84632ccd9deb31
Frolman <frolfrol8383@gmail.com>
2024-12-04 12:51:10 +0300

    Добавил видео

--- plates.py
+++ plates.py
@@ -3,17 +3,17 @@
 import cv2
 
 #Загрузка каскада
-cascad_src = 'haarcascade_frontalface_alt.xml'
+cascad_src = 'haarcascade_russian_plate_number.xml'
 face_cascad = cv2.CascadeClassifier(cascad_src)
 
 #Загрузка изображений
-image = cv2.imread ('face_4.jpeg')
+image = cv2.imread ('car_2.jpg')
 
 #Перевод в градации серого
 grey_image = cv2.cvtColor (image, cv2.COLOR_BGR2GRAY)
 
 #Обнаружение лиц на изображении
-faces = face_cascad.detectMultiScale(grey_image, scaleFactor=1.01, minNeighbors=2,minSize = (100,100))
+faces = face_cascad.detectMultiScale(grey_image, scaleFactor=1.01, minNeighbors=2,minSize = (20,20))
 
 #Выделение распознанных областей
 for (x,y,w,h) in faces:
---
+++ transparent_text.py
@@ -0,0 +1,41 @@
+
+# Python program to write 
+# text on video 
+  
+  
+import cv2 
+  
+  
+cap = cv2.VideoCapture('video.avi') 
+  
+while(True): 
+      
+    # Capture frames in the video 
+    ret, frame = cap.read() 
+  
+    # describe the type of font 
+    # to be used. 
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+  
+    # Use putText() method for 
+    # inserting text on video 
+    cv2.putText(frame,  
+                'FROLMAN',  
+                (20, 230),  
+                font, 1,  
+                (255, 0, 0),2,  
+                cv2.LINE_4) 
+                 
+  
+    # Display the resulting frame 
+    cv2.imshow('video', frame) 
+  
+    # creating 'q' as the quit  
+    # button for the video 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
+  
+# release the cap object 
+cap.release() 
+# close all windows 
+cv2.destroyAllWindows()
